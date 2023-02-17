a5661b53776306da96ff6e155d7f95a9056d461c
elizakanska <78021756+elizakanska@users.noreply.github.com>
2023-02-17 21:56:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,32 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+        elif next in ")]}":
+            #Tukšs steks, nav jēga tālāk skatīties
+            if not opening_brackets_stack:
+                return i
+            top = opening_brackets_stack.pop()
+            #Pārbauda vai aizverošā iekava sakrīt ar atverošo iekavu
+            if not are_matching(top.char, next):
+                return i
+     
+    #Ja steks vēl nav tukšs, tad beigas ir neaizvērtas iekavas
+     if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    
+    return -1
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    #ja nav bijusi kļūme, tās pozīcija būs negatīva(ārpus teksta robežām)
+    if mismatch == -1:
+        print("Success")
+    else:
+        print(mismatch + 1)
 
 
 if __name__ == "__main__":
